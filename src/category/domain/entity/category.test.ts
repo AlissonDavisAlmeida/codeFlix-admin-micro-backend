05a165b95ca5bfaf77ad34b0537c8e8175082241
Alisson <alissoneatena1@gmail.com>
2022-07-09 00:16:32 -0300

    set properties in entity Category

--- src/category/domain/entity/category.test.ts
+++ src/category/domain/entity/category.test.ts
@@ -1,14 +1,16 @@
 import { Category } from "./category";
 
 
-describe("Category unit test",()=>{
+describe("Category unit test", () => {
 
-    it("should be created",()=>{
-        const category = new Category(1,"test","test");
-        
+    it("should be created a new Category", () => {
+        const category = new Category({ name: "test", description: "test", isActive: true });
+
         
         expect(category).toBeTruthy();
         expect(category.name).toBe("test");
+        expect(category.description).toBe("test");
+        expect(category.isActive).toBe(true);
 
     });
 
--- src/category/domain/entity/category.ts
+++ src/category/domain/entity/category.ts
@@ -1,3 +1,9 @@
+interface CategoryState {
+    readonly name: string;
+    readonly description?: string;
+    readonly isActive?: boolean;
+}
+
 export class Category {
 
     #id: number;
@@ -7,10 +13,13 @@ export class Category {
     #updatedAt: Date;
 
 
-    constructor(id: number, name: string, description: string) {
-        this.#id = id;
+    constructor(
+        { name, description, isActive }: CategoryState,
+    ) {
+
+        this.#id = 0;
         this.#name = name;
-        this.#description = description;
+        this.#description = description || "";
         this.#createdAt = new Date();
         this.#updatedAt = new Date();
     }
@@ -18,4 +27,21 @@ export class Category {
     get name(): string {
         return this.#name;
     }
+
+    get description(): string {
+        return this.#description;
+    }
+
+    get isActive(): boolean {
+        return true;
+    }
+    
+    get id(): number {
+        return this.#id;
+    }
+
+    get createdAt(): Date {
+        return this.#createdAt;
+    }
+
 }
