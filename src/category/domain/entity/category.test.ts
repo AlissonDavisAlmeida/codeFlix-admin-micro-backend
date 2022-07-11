a5615fb8dc6e80eec3c4ad69fab3e5022d1f17aa
Alisson <alissoneatena1@gmail.com>
2022-07-11 00:00:02 -0300

    tests logic category

--- src/category/domain/entity/category.test.ts
+++ src/category/domain/entity/category.test.ts
@@ -4,14 +4,36 @@ import { Category } from "./category";
 describe("Category unit test", () => {
 
     it("should be created a new Category", () => {
-        const category = new Category({ name: "test", description: "test", isActive: true });
+        const state = {
+            name: "Test",
+            description: "Test",
+            isActive: true,
+        }
 
-        
+        const category = new Category(state);
+
         expect(category).toBeTruthy();
-        expect(category.name).toBe("test");
-        expect(category.description).toBe("test");
-        expect(category.isActive).toBe(true);
+        expect(category.getProps()).toStrictEqual({
+            name: "Test",
+            description: "Test",
+            isActive: true,
+        })
+        expect(category.id).toBe(0);
+        expect(category.createdAt).toBeInstanceOf(Date);
 
     });
+
+    it("Should be created a new Category with default values", () => {
+        const category = new Category({ name: "Test" });
+
+        expect(category).toBeTruthy();
+        expect(category.getProps()).toStrictEqual({
+            name: "Test",
+            description: "",
+            isActive: true,
+        })
+        expect(category.id).toBe(0);
+        expect(category.createdAt).toBeInstanceOf(Date);
+    })
 
 })
--- src/category/domain/entity/category.ts
+++ src/category/domain/entity/category.ts
@@ -8,10 +8,9 @@ export class Category {
 
     #id: number;
     #name: string;
-    #description: string;
+    #description?: string;
     #createdAt: Date;
-    #updatedAt: Date;
-
+    #isActive?: boolean;
 
     constructor(
         { name, description, isActive }: CategoryState,
@@ -19,9 +18,17 @@ export class Category {
 
         this.#id = 0;
         this.#name = name;
-        this.#description = description || "";
+        this.description = description;
+        this.isActive = isActive
         this.#createdAt = new Date();
-        this.#updatedAt = new Date();
+    }
+
+    private set description(description: string | undefined) {
+        this.#description = description ?? "";
+    }
+
+    private set isActive(isActive: boolean | undefined) {
+        this.#isActive = isActive || true;
     }
 
     get name(): string {
@@ -29,19 +36,27 @@ export class Category {
     }
 
     get description(): string {
-        return this.#description;
+        return this.#description ?? "";
     }
 
     get isActive(): boolean {
-        return true;
+        return this.#isActive!;
     }
-    
+
     get id(): number {
         return this.#id;
     }
 
     get createdAt(): Date {
         return this.#createdAt;
+    }
+
+    getProps(): CategoryState {
+        return {
+            name: this.#name,
+            description: this.#description,
+            isActive: this.#isActive,
+        }
     }
 
 }
