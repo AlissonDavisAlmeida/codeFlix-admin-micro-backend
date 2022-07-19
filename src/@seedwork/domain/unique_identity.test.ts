cbaf3475ce4401e5f5eda6d0c20130477a95cd6e
Alisson <alissoneatena1@gmail.com>
2022-07-19 02:27:24 +0000

    remote container

--- src/@seedwork/domain/unique_identity.test.ts
+++ src/@seedwork/domain/unique_identity.test.ts
@@ -4,32 +4,32 @@ import { UniqueIdentity } from "./unique_identity"
 
 describe("Unique identity unit test", () => {
 
+    const spyValidateMethod = jest.spyOn(UniqueIdentity.prototype as any, "validate")
+
+
     it("should throw error when uuid is not valid", () => {
-        const validateSpy = jest.spyOn(UniqueIdentity.prototype as any, "validate")
         expect(() => new UniqueIdentity("fake-uuid")).toThrowError(InvalidUuidError)
-        expect(validateSpy).toHaveBeenCalled()
-        expect(validateSpy).toHaveBeenCalledTimes(1)
+        expect(spyValidateMethod).toHaveBeenCalled()
+        expect(spyValidateMethod).toHaveBeenCalledTimes(1)
 
     })
 
     it("should accept a uuid passed in constructor", ()=>{
 
-        const validateSpy = jest.spyOn(UniqueIdentity.prototype as any, "validate")
         const id = "0fdb4026-4df1-4522-96d2-27c6ce868212"
         const identityVO = new UniqueIdentity("0fdb4026-4df1-4522-96d2-27c6ce868212")
 
         expect(identityVO.id).toBe(id)
-        expect(validateSpy).toHaveBeenCalled()
-        expect(validateSpy).toHaveBeenCalledTimes(1)
+        expect(spyValidateMethod).toHaveBeenCalled()
+        expect(spyValidateMethod).toHaveBeenCalledTimes(1)
     })
 
     it("should generate a uuid when no uuid is passed in constructor", ()=>{
             
-            const validateSpy = jest.spyOn(UniqueIdentity.prototype as any, "validate")
             const identityVO = new UniqueIdentity()
     
             expect(validate(identityVO.id)).toBeTruthy()
-            expect(validateSpy).toHaveBeenCalled()
-            expect(validateSpy).toHaveBeenCalledTimes(1)
+            expect(spyValidateMethod).toHaveBeenCalled()
+            expect(spyValidateMethod).toHaveBeenCalledTimes(1)
     })
 })
