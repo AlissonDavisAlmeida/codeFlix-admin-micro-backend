493ede1a7871d085be734c51d3a81e449b8579bb
Alisson <alissoneatena1@gmail.com>
2022-08-15 23:29:29 -0300

    add in memory repository

---
+++ src/@seedwork/domain/repository/inMemory.repository.ts
@@ -0,0 +1,52 @@
+import { BaseEntity } from "../entity/BaseEntity";
+import { NotFoundError } from "../errors/not-found.error";
+import { UniqueIdentity } from "../valueObjects/unique_identity";
+import { RepositoryInterface } from "./repository.interface";
+
+export abstract class InMemoryRepository<T extends BaseEntity> implements RepositoryInterface<T> {
+  items: T[] = [];
+
+  async findById(id: string | UniqueIdentity): Promise<T> {
+    const idformatted = `${id}`;
+    const itemDiscovery = await this.getItemFromId(idformatted);
+
+    return itemDiscovery;
+  }
+
+  async findAll(): Promise<T[]> {
+    return Promise.resolve(this.items);
+  }
+
+  async save(entity: T): Promise<T> {
+    this.items.push(entity);
+
+    return Promise.resolve(entity);
+  }
+
+  async update(entity: T): Promise<T> {
+    let foundItem = await this.getItemFromId(entity.id);
+
+    foundItem = structuredClone(entity);
+
+    this.items = this.items.map((item: T) => (item.id === foundItem.id ? foundItem : item));
+
+    return Promise.resolve(foundItem);
+  }
+
+  async delete(id: string | UniqueIdentity): Promise<void> {
+    const idformatted = `${id}`;
+    await this.getItemFromId(idformatted);
+
+    this.items = this.items.filter((item: T) => item.id !== idformatted);
+  }
+
+  private async getItemFromId(id: string): Promise<T> {
+    const itemDiscovery = this.items.find((item: T) => item.id === id);
+
+    if (!itemDiscovery) {
+      throw new NotFoundError(`Item not found with id: ${id}`);
+    }
+
+    return itemDiscovery;
+  }
+}
--- src/@seedwork/domain/repository/repository.interface.ts
+++ src/@seedwork/domain/repository/repository.interface.ts
@@ -3,7 +3,7 @@ import { UniqueIdentity } from "../valueObjects/unique_identity";
 
 export interface RepositoryInterface<T extends BaseEntity> {
 
-  findById(id: string): Promise<T>;
+  findById(id: string | UniqueIdentity): Promise<T>;
   findAll(): Promise<T[]>;
   save(entity: T): Promise<T>;
   update(entity: T): Promise<T>;
