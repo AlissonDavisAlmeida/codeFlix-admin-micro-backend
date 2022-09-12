ce5fb904bbea785a1750a35187d9cb01e2c63a36
Alisson <alissoneatena1@gmail.com>
2022-09-11 22:42:39 -0300

    add tests to category repository inMemory

--- src/category/infra/repository/category-repositoryInMemory.ts
+++ src/category/infra/repository/category-repositoryInMemory.ts
@@ -3,11 +3,18 @@ import { Category } from "../../domain/entity/category";
 import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/inMemory.repository";
 
 export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category> implements CategoryRepository.Repository {
+  sortableFields: string[] = ["name", "createdAt", "updated_at"];
+
   protected async applyFilter(items: Category[], filter: CategoryRepository.Filter): Promise<Category[]> {
     if (!filter) {
       return items;
     }
 
     return items.filter((item) => item.props.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  protected async applySort(items: Category[], sort: string | null, sort_dir: "asc" | "desc" | null): Promise<Category[]> {
+    return !sort ? super.applySort(items, "createdAt", "desc")
+      : super.applySort(items, sort, sort_dir);
   }
 }
