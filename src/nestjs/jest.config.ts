9e98d679137abd5b7759bd543197378340964ecb
Alisson <alissoneatena1@gmail.com>
2022-12-29 21:41:15 -0300

    add resource categories in nestjs

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -13,7 +13,7 @@ export default {
     rootDir: "src",
     testRegex: ".*\\.spec\\.ts$",
     transform: {
-        "^.+\\.(t|j)s$": "ts-jest",
+        "^.+\\.(t|j)s$": "@swc/jest",
 
     },
     collectCoverageFrom: [
