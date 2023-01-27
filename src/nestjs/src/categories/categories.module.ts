6970192f0a9964e6e9d7086ee2fc64bfa72255aa
Alisson <alissoneatena1@gmail.com>
2023-01-27 09:20:05 -0300

    updates changes

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,53 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-import { CreateCategory, DeleteCategory, GetCategory, ListCategories, UpdateCategory } from "micro-videos-core/category/application"
-import {CategoryRepositoryInMemory} from "micro-videos-core/category/infra"
-import { CategoryRepository } from 'micro-videos-core/dist/category/domain/repositories/category-repository';
+import { CategoriesService } from './categories.service';
+import { CATEGORY_PROVIDERS } from './category.providers';
 
 @Module({
   controllers: [CategoriesController],
   providers: [
     CategoriesService,
-    {
-      provide: "CategoryInMemoryRepository",
-      useClass: CategoryRepositoryInMemory
-    },
-    {
-      provide: CreateCategory,
-      useFactory: (categoryRepository: CategoryRepository.Repository)=>{
-        return new CreateCategory(categoryRepository)
-      },
-      inject: ["CategoryInMemoryRepository"]
-    },
-    {
-      provide: ListCategories,
-      useFactory: (categoryRepository: CategoryRepository.Repository)=>{
-        return new ListCategories(categoryRepository)
-      },
-      inject: ["CategoryInMemoryRepository"]
-    },
-    {
-      provide: GetCategory,
-      useFactory: (categoryRepository: CategoryRepository.Repository)=>{
-        return new GetCategory(categoryRepository)
-      },
-      inject: ["CategoryInMemoryRepository"]
-    },
-    {
-      provide: UpdateCategory,
-      useFactory: (categoryRepository: CategoryRepository.Repository)=>{
-        return new UpdateCategory(categoryRepository)
-      },
-      inject: ["CategoryInMemoryRepository"]
-    },
-    {
-      provide: DeleteCategory,
-      useFactory: (categoryRepository: CategoryRepository.Repository)=>{
-        return new DeleteCategory(categoryRepository)
-      },
-      inject: ["CategoryInMemoryRepository"]
-    }
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES)
   ]
 })
 
