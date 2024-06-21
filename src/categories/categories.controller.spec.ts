c97fafbf03cddc5518d4a598662d68b3b0c384e7
Alisson <alissoneatena1@gmail.com>
2024-06-21 16:59:19 +0000

    feat: create a database module

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { CategoriesController } from "./categories.controller";
 import { CategoryModel, CategoryRepositorySequelize } from "@core/category/infra/db/sequelize";
 import { SequelizeModule, getModelToken } from "@nestjs/sequelize";
+import { DatabaseModule } from "src/database/database.module";
 
 describe("CategoriesController", () => {
   let controller: CategoriesController;
@@ -9,12 +10,7 @@ describe("CategoriesController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        SequelizeModule.forRoot({
-          dialect: "sqlite",
-          host: ":memory:",
-          logging: false,
-          models: [CategoryModel],
-        }),
+        DatabaseModule,
         SequelizeModule.forFeature([CategoryModel]),
       ],
       controllers: [CategoriesController],
