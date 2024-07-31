917b54870b3ef12b03e592a24330b920e3fd2473
Alisson <alissoneatena1@gmail.com>
2024-07-31 16:24:42 +0000

    refactor: changing categories folder name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "./nest-modules/config/config.module";
 import { DatabaseModule } from "./nest-modules/database/database.module";
-import { CategoriesModule } from "./nest-modules/categories/categories.module";
+import { CategoriesModule } from "./nest-modules/categories-module/categories.module";
 
 @Module({
   imports: [
